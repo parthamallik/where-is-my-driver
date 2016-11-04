368f797bb4b586982b868459d548085af55124ae
Partha <mallik.partha@gmail.com>
2016-11-04 09:51:06 +0530

    Adding tests for driver api

---
+++ test/drivers_data.js
@@ -0,0 +1,17 @@
+exports.seed = [
+    {
+        'id': 1,
+        'longitude': 50.311,
+        'latitude': 13.45
+    },
+    {
+        'id': 2,
+        'longitude': 50.312,
+        'latitude': 13.45
+    },
+    {
+        'id': 3,
+        'longitude': 50.312,
+        'latitude': 13.44
+    }
+];
